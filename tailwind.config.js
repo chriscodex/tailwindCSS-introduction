a41cc8584a0b4e07a8e7cb4f043883a44be247e8
Christian <c.espinoza.code@gmail.com>
2023-06-01 19:40:21 -0500

    flexbox html test file deleted from tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/css/index.css", "./src/Teoria/flexbox-row.html"],
+  content: ["./public/index.html", "./src/css/index.css"],
   theme: {
     // colors: {
     //   'white': '#ffffff',
@@ -8,7 +8,7 @@ module.exports = {
     //   'xd': '#121063',
     //   'metal': '#565584',
     //   'tahiti': '#3ab7bf',
-    //   'silver': '#ecebff',
+    //   'silver': '#ecebff', 
     //   'bubble-gum': '#ff77e9',
     //   'bermuda': '#78dcca',
     // },
