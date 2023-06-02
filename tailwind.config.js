8d4e77c76957ac9de8c18502a779edbe39765c06
Christian <c.espinoza.code@gmail.com>
2023-06-01 19:39:04 -0500

    tailwind config file generated

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/index.html", "./src/css/index.css", "./src/Teoria/flexbox-row.html"],
+  theme: {
+    // colors: {
+    //   'white': '#ffffff',
+    //   'purple': '#3f3cbb',
+    //   'xd': '#121063',
+    //   'metal': '#565584',
+    //   'tahiti': '#3ab7bf',
+    //   'silver': '#ecebff',
+    //   'bubble-gum': '#ff77e9',
+    //   'bermuda': '#78dcca',
+    // },
+    extend: {}
+  },
+  plugins: [],
+}
+
