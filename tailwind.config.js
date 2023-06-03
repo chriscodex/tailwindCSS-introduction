79206972b1efbc181ce2805b531dad80046ff43b
Christian <c.espinoza.code@gmail.com>
2023-06-02 19:53:26 -0500

    images added as properties in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,25 @@
 module.exports = {
   content: ["./public/index.html", "./src/css/index.css"],
   theme: {
-    extend: {}
+    extend: {
+      backgroundImage: {
+        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
+			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
+			  'yosemite': "url('../img/yosemite.jpg')",
+			  'LA': "url('../img/LA.jpg')",
+			  'seattle': "url('../img/seattle.jpg')",
+			  'new_york': "url('../img/new_york.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'sydney': "url('../img/sydney.jpg')",
+			  'miami': "url('../img/miami.jpg')",
+			  'switzerland': "url('../img/switzerland.jpg')",
+			  'bali': "url('../img/bali.jpg')",
+			  'norway': "url('../img/norway.jpg')",
+			  'chicago': "url('../img/chicago.jpg')",
+			  'europe': "url('../img/europe.jpg')",
+			  'iceland': "url('../img/iceland.jpg')",
+      }
+    }
   },
   plugins: [],
 }
-
