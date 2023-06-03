3c1145870a2c17d7c8a82dd7e61b0ac3c3ecffaa
Christian <c.espinoza.code@gmail.com>
2023-06-02 19:46:00 -0500

    comments deleted from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   content: ["./public/index.html", "./src/css/index.css"],
   theme: {
-    // colors: {
-    //   'white': '#ffffff',
-    //   'purple': '#3f3cbb',
-    //   'xd': '#121063',
-    //   'metal': '#565584',
-    //   'tahiti': '#3ab7bf',
-    //   'silver': '#ecebff', 
-    //   'bubble-gum': '#ff77e9',
-    //   'bermuda': '#78dcca',
-    // },
     extend: {}
   },
   plugins: [],
