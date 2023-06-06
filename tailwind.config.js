706090ad1ff14a567ea2047005589b0710027203
Christian <c.espinoza.code@gmail.com>
2023-06-06 15:47:21 -0500

    useless cuotes deleted

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/index.html", "./src/css/index.css"],
+  content: ['./public/index.html', './src/css/index.css'],
   theme: {
     extend: {
       backgroundImage: {
-        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
-			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
-			  'yosemite': "url('../img/yosemite.jpg')",
-			  'LA': "url('../img/LA.jpg')",
-			  'seattle': "url('../img/seattle.jpg')",
-			  'new_york': "url('../img/new_york.jpg')",
-			  'norway': "url('../img/norway.jpg')",
-			  'sydney': "url('../img/sydney.jpg')",
-			  'miami': "url('../img/miami.jpg')",
-			  'switzerland': "url('../img/switzerland.jpg')",
-			  'bali': "url('../img/bali.jpg')",
-			  'norway': "url('../img/norway.jpg')",
-			  'chicago': "url('../img/chicago.jpg')",
-			  'europe': "url('../img/europe.jpg')",
-			  'iceland': "url('../img/iceland.jpg')",
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        norway: "url('../img/norway.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
       },
-      backgroundColor: theme => ({
+      backgroundColor: (theme) => ({
         ...theme('colors'),
-        'primary': "#CC2D4A",
-        'secondary': "#8FA206",
-        'terciary': "#61AEC9",
+        primary: '#CC2D4A',
+        secondary: '#8FA206',
+        terciary: '#61AEC9',
       }),
       textColor: {
-        'primary': "#CC2D4A",
-        'secondary': "#8FA206",
-        'terciary': "#61AEC9",
+        primary: '#CC2D4A',
+        secondary: '#8FA206',
+        terciary: '#61AEC9',
       },
       fontFamily: {
-        Montserrat: ['"Montserrat"', "sans-serif"],
-      }
+        Montserrat: ['"Montserrat"', 'sans-serif'],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
